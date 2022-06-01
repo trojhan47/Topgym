83c9673f5469e692fb3aa0d0e6fceba2ead77f1a
Hanifat Owuri <neefah300@gmail.com>
2022-06-01 14:34:06 +0100

    created custom style files for antd theme

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -10,16 +10,25 @@ module.exports = {
       options: {
         customizeTheme: {
           "@primary-color": "#43E0CB",
+          "@primary-color-opaque": "#43E0CB1A",
           "@primary-1": "#000000",
           "@text-color": "#FFFFFF",
           "@component-background": "#ffffff00",
           //header menu-item
           "@menu-item-vertical-margin": "8px",
-          "@input-bg": "#43E0CB1A",
+          "@input-bg": "@primary-color-opaque",
           "@input-color": "@primary-1",
           "@input-placeholder-color": "@primary-1",
           "@input-padding": "8px 12px",
           "@menu-dark-inline-submenu-item-color": "#FFFF00",
+          "@menu-dark-inline-submenu-item-bg": "@primary-1",
+          "@menu-item-color": "@primary-1",
+          "@menu-item-font-size": "16px",
+          "@select-background": "@primary-color-opaque",
+          "@select-dropdown-bg": "@primary-1",
+          "@select-item-color": "@text-color",
+          "@select-item-font-size": "16px",
+          "@select-item-padding": "8px 12px",
         },
         lessLoaderOptions: {
           lessOptions: {
--- frontend/src/pages/Subscription/index.tsx
+++ frontend/src/pages/Subscription/index.tsx
@@ -1,5 +1,102 @@
 import React from "react";
+import { Form, Select, Button, Space, Row, Col } from "antd";
 
-const index = () => <h1>SUBSCRIBE TO OUR PLANS</h1>;
+import aboutStyle from "../About/style.module.scss";
+import contactStyle from "../Contact/style.module.scss";
+import style from "./style.module.scss";
+
+const { Option } = Select;
+
+const onFinish = (values: any) => {
+  console.log("Success:", values);
+};
+
+const onFinishFailed = (errorInfo: any) => {
+  console.log("Failed:", errorInfo);
+};
+
+const handleChange = (value: string) => {
+  console.log(`selected ${value}`);
+};
+
+const index = () => (
+  <Row className={style.subRow}>
+    <Col xs={0} sm={0} md={0} lg={12} className={style.leftCol}>
+      <img
+        className={style.image}
+        alt="subBackground"
+        width={400}
+        height={500}
+        src={`${process.env.PUBLIC_URL}/resources/images/couple_workout.png`}
+      />
+    </Col>
+    <Col xs={24} sm={24} md={24} lg={12} className={style.formContainer}>
+      <div className={style.flex}>
+        <img
+          src={`${process.env.PUBLIC_URL}/resources/images/Vector.svg`}
+          alt="topgymIcon"
+          width={40}
+        />
+        <h2 className={style.header}>TopGym</h2>
+      </div>
+
+      <div className={style.header}>
+        <h2 className={style.hText}>Subscribe to enjoy our amazing Features</h2>
+      </div>
+
+      <Form
+        name="basic"
+        wrapperCol={{ span: 24 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Space direction="vertical" size="middle">
+          <Form.Item
+            rules={[
+              { required: true, message: "please pick an account type!" },
+            ]}
+          >
+            <Select
+              defaultValue="Choose an account type"
+              style={{ width: 300 }}
+              onChange={handleChange}
+            >
+              <Option value="single">Single</Option>
+              <Option value="couple">Couple</Option>
+              <Option value="family">Family</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            rules={[{ required: true, message: "please choose a plan!" }]}
+            style={{ height: 120, padding: 5 }}
+          >
+            <Select
+              defaultValue="Choose your subscription plan"
+              style={{ width: 300 }}
+              onChange={handleChange}
+            >
+              <Option value="monthly">1 Month</Option>
+              <Option value="quaterly">6 Months</Option>
+              <Option value="yearly">12 Months</Option>
+            </Select>
+          </Form.Item>
+        </Space>
+
+        <Form.Item wrapperCol={{ span: 24 }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className={style.Button}
+            style={{ padding: "10px", height: "40px" }}
+          >
+            Proceed to payment
+          </Button>
+        </Form.Item>
+      </Form>
+    </Col>
+  </Row>
+);
 
 export default index;
